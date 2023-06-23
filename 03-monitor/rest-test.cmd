7c010062a90c1e6ddba042b7444fc7d263fa2685
Emad Saber Hamed <omda.sharkawy@live.com>
2023-06-23 03:09:21 +0300

    Lab 03 Done

--- 03-monitor/rest-test.cmd
+++ 03-monitor/rest-test.cmd
@@ -1,1 +1,1 @@
-curl -X POST "<yourEndpoint>/text/analytics/v3.1/languages?" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: <yourKey>" --data-ascii "{'documents':[{'id':1,'text':'hello'}]}"
+curl -X POST "https://ai-svc.cognitiveservices.azure.com/text/analytics/v3.1/languages?" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: 5895e6a3660e48eab210e21a02d699cc" --data-ascii "{'documents':[{'id':1,'text':'hello'}]}"
