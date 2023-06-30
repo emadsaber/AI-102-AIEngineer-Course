557fbd6f1fda35a094f29296884779caed79e207
Emad Saber Hamed <omda.sharkawy@live.com>
2023-07-01 02:08:06 +0300

    Lab 09 - Done

--- 09-luis-app/GetIntent.cmd
+++ 09-luis-app/GetIntent.cmd
@@ -1,13 +1,16 @@
-@echo off
+@REM @echo off
 
-rem Set values for your Language Understanding app
-set app_id=YOUR_APP_ID
-set endpoint=YOUR_ENDPOINT
-set key=YOUR_KEY
+@REM rem Set values for your Language Understanding app
+@REM set app_id=YOUR_APP_ID
+@REM set endpoint=YOUR_ENDPOINT
+@REM set key=YOUR_KEY
 
-rem Get parameter and encode spaces for URL
-set input=%1
-set query=%input: =+%
+@REM rem Get parameter and encode spaces for URL
+@REM set input=%1
+@REM set query=%input: =+%
 
-rem Use cURL to call the REST API
-curl -X GET "%endpoint%/luis/prediction/v3.0/apps/%app_id%/slots/production/predict?subscription-key=%key%&log=true&query=%query%"
+@REM rem Use cURL to call the REST API
+@REM curl -X GET "%endpoint%/luis/prediction/v3.0/apps/%app_id%/slots/production/predict?subscription-key=%key%&log=true&query=%query%"
+
+
+curl -X POST "https://ai-language-svc-001.cognitiveservices.azure.com/language/:analyze-conversations?api-version=2022-10-01-preview" -H "Ocp-Apim-Subscription-Key: 712470ac96094a6193b617297ed33621"  -H "Apim-Request-Id: 4ffcac1c-b2fc-48ba-bd6d-b69d9942995a" -H "Content-Type: application/json" -d "{\"kind\":\"Conversation\",\"analysisInput\":{\"conversationItem\":{\"id\":\"1\",\"text\":\"What date will it be in New Delhi on Wednesday?\",\"modality\":\"text\",\"language\":\"en\",\"participantId\":\"1\"}},\"parameters\":{\"projectName\":\"lab09\",\"verbose\":true,\"deploymentName\":\"TimeModelDep\",\"stringIndexType\":\"TextElement_V8\"}}"
