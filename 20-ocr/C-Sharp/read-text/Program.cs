b08246dacd989c2dd60af7f1b793ed88201fc16e
Emad Saber Hamed <omda.sharkawy@live.com>
2023-08-08 01:24:53 +0300

    Lab 21

--- 20-ocr/C-Sharp/read-text/Program.cs
+++ 20-ocr/C-Sharp/read-text/Program.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 
 // Import namespaces
 
@@ -14,24 +16,30 @@ namespace read_text
 {
     class Program
     {
-
         private static ComputerVisionClient cvClient;
+
         static async Task Main(string[] args)
         {
             try
             {
                 // Get config settings from AppSettings
-                IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+                IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile(
+                    "appsettings.json"
+                );
                 IConfigurationRoot configuration = builder.Build();
                 string cogSvcEndpoint = configuration["CognitiveServicesEndpoint"];
                 string cogSvcKey = configuration["CognitiveServiceKey"];
 
                 // Authenticate Computer Vision client
-                
-
+                ApiKeyServiceClientCredentials credentials = new ApiKeyServiceClientCredentials(
+                    cogSvcKey
+                );
+                cvClient = new ComputerVisionClient(credentials) { Endpoint = cogSvcEndpoint };
 
                 // Menu for text reading functions
-                Console.WriteLine("1: Use Read API for image\n2: Use Read API for document\n3: Read handwriting\nAny other key to quit");
+                Console.WriteLine(
+                    "1: Use Read API for image\n2: Use Read API for document\n3: Read handwriting\nAny other key to quit"
+                );
                 Console.WriteLine("Enter a number:");
                 string command = Console.ReadLine();
                 string imageFile;
@@ -52,7 +60,6 @@ namespace read_text
                     default:
                         break;
                 }
-
             }
             catch (Exception ex)
             {
@@ -63,8 +70,44 @@ namespace read_text
         static async Task GetTextRead(string imageFile)
         {
             Console.WriteLine($"Reading text in {imageFile}\n");
+            // Use Read API to read text in image
+            using (var imageData = File.OpenRead(imageFile))
+            {
+                var readOp = await cvClient.ReadInStreamAsync(imageData);
 
-     
+                // Get the async operation ID so we can check for the results
+                string operationLocation = readOp.OperationLocation;
+                string operationId = operationLocation.Substring(operationLocation.Length - 36);
+
+                // Wait for the asynchronous operation to complete
+                ReadOperationResult results;
+                do
+                {
+                    Thread.Sleep(1000);
+                    results = await cvClient.GetReadResultAsync(Guid.Parse(operationId));
+                } while (
+                    (
+                        results.Status == OperationStatusCodes.Running
+                        || results.Status == OperationStatusCodes.NotStarted
+                    )
+                );
+
+                // If the operation was successfully, process the text line by line
+                if (results.Status == OperationStatusCodes.Succeeded)
+                {
+                    var textUrlFileResults = results.AnalyzeResult.ReadResults;
+                    foreach (ReadResult page in textUrlFileResults)
+                    {
+                        foreach (Line line in page.Lines)
+                        {
+                            Console.WriteLine(line.Text);
+
+                            // Uncomment the following line if you'd like to see the bounding box
+                            //Console.WriteLine(line.BoundingBox);
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- 21-custom-form/setup.cmd
+++ 21-custom-form/setup.cmd
@@ -2,9 +2,9 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 rem Set variable values 
-set subscription_id=YOUR_SUBSCRIPTION_ID
-set resource_group=YOUR_RESOURCE_GROUP
-set location=YOUR_LOCATION_NAME
+set subscription_id=44ec6692-568c-4aa2-894d-f00a2b5930fc
+set resource_group=ai-rg
+set location=eastus
 set expiry_date=2024-01-01T00:00:00Z
 
 rem Get random numbers to create unique resource names
